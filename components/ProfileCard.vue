d25431f9277d984f7f6ce7e31511f06ef631a5e0
Yuko_Yoshida <yukoyoshida614@gmail.com>
2019-10-16 17:59:00 +0900

    grass #25

--- components/ProfileCard.vue
+++ components/ProfileCard.vue
@@ -1,54 +1,43 @@
 <template>
   <section class="section">
-    <div class="card article">
-      <div class="media">
-        <div class="media-left">
-          <figure class="image is-48x48">
-            <img :src="$store.state.main.profile.avatar_url" alt="Placeholder image">
+    <div class="card article" id="profile">
+      <a :href="$store.state.main.profile.html_url">
+        <div class="card-content">
+          <div class="media">
+            <div class="media-left">
+              <figure class="image is-96x96">
+                <img class="is-rounded" :src="$store.state.main.profile.avatar_url" alt="Avatar image">
+              </figure>
+            </div>
+            <div class="media-content" id="name">
+              <p class="title is-4">{{ $store.state.main.profile.name }}</p>
+              <p class="subtitle is-6">@{{ $store.state.main.profile.login }}</p>
+            </div>
+          </div>
+
+          <div class="content">
+            <p class="has-text-black is-5">{{ $store.state.main.profile.bio }}</p>
+          </div>
+        </div>
+
+        <footer class="card-footer">
+          <figure class="image">
+            <img id="grass" :src="`https://grass-graph.moshimo.works/images/${$store.state.main.profile.login}.png`">
           </figure>
-        </div>
-        <div class="media-content">
-          <p class="title is-4">{{ $store.state.main.profile.name }}</p>
-          <p class="subtitle is-6">@{{ $store.state.main.profile.login }}</p>
-        </div>
-      </div>
-
-      <div class="content">
-        {{ $store.state.main.profile.bio }}
-      </div>
-
-      <footer class="card-footer">
-        <a :href="$store.state.main.profile.html_url" class="card-footer-item">GitHub</a>
-      </footer>
+        </footer>
+      </a>
     </div>
   </section>
 </template>
 
-<script>
-export default {
-  // props: {
-  //   profile: {
-  //     name: {
-  //       type: String,
-  //       required: true
-  //     },
-  //     login: {
-  //       type: String,
-  //       required: true
-  //     },
-  //     bio: {
-  //       type: String,
-  //       required: true
-  //     },
-  //     avatar_url: {
-  //       type: String,
-  //       required: true
-  //     },
-  //     html_url: {
-  //       type: String,
-  //       required: true
-  //     }
-  //   }
-  // }
+<style>
+#profile {
+  transition: box-shadow 0.3s;
 }
-</script>
+#profile:hover {
+  box-shadow: 2px 2px 4px 0px rgba(0,0,0,0.5);
+}
+#name {
+  margin: auto;
+}
+</style>
