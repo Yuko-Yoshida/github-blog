d0d43bc62464e876a09c210ae2600fecf57258ef
Yuko_Yoshida <yukoyoshida614@gmail.com>
2019-10-18 02:20:38 +0900

    pass token #53

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-echo "module.exports = { repoUrl: '$1', token: '$2', blogTitle: '$3', deployRepoUrl: '$4' }" > env.production.js
+echo "module.exports = { repoUrl: '$1', blogTitle: '$2', deployRepoUrl: '$3' }" > env.production.js
